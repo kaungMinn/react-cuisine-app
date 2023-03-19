f6773580117774ec506abfb002e512bd1d4c7f25
kaung150 <kmkhant1500@gmail.com>
2023-03-19 09:52:58 +0630

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import Category from "./components/Category";
+import Pages from "./pages/Pages";
+import { BrowserRouter } from 'react-router-dom';
+import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from 'react-icons/gi';
+
+const App = props => {
+    return (
+        <div className='App'>
+            <BrowserRouter>
+                <Nav>
+                    <GiKnifeFork />
+                    <Logo to={"/"} >Kaung Min Cuisines</Logo>
+                </Nav>
+                <Search />
+                <Category />
+                <Pages />
+            </BrowserRouter>
+        </div>
+    );
+}
+
+const Logo = styled(Link)`
+    text-decoration: none;
+    font-size: 1.5rem;
+    font-weight: 400;
+    font-family: 'Lobster Two',cursive;
+
+`;
+
+const Nav = styled.div`
+    padding: 4rem 0rem;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+
+    svg{
+        font-size: 2rem;
+    }
+`;
+
+
+
+export default App;
